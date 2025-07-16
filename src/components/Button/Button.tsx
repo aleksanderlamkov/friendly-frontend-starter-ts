6c46f7384936caaee859b6fe5ed7b1423f963501
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2025-07-16 15:40:07 +0300

    refactor(Button): изменил имена вспомогательных переменных на более подходящие

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,12 +7,12 @@ export default (props: TButtonProps) => {
 
   const isLink = href !== undefined
   const Component = isLink ? 'a' : 'button'
-  const linkProps = { href }
-  const buttonProps = { type }
-  const specificProps = isLink ? linkProps : buttonProps
+  const linkAttributes = { href }
+  const buttonAttributes = { type }
+  const attributesByTag = isLink ? linkAttributes : buttonAttributes
 
   return (
-    <Component className={clsx('button', className)} {...specificProps}>
+    <Component className={clsx('button', className)} {...attributesByTag}>
       {children}
     </Component>
   )

a74152095801f6e8701b04b0a10ed6184d8943c1
Aleksander Lamkov <aleksanderlamkov@gmail.com>
2025-07-16 11:06:28 +0300

    docs: добавлена ссылка на PostCSS-плагины в `postcss.config.js`

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,9 @@ import postcssPresetEnv from 'postcss-preset-env'
 
 export default ({ env }) => {
   const isProd = env === 'production'
+  /**
+   * @see https://postcss.org/docs/postcss-plugins
+   */
   const plugins = []
 
   if (isProd) {
